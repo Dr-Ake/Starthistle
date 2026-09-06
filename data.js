2efac6914a3e8bd1249daa0fdcb0557b6a6c3833
Dr-Ake <Drakefreeman@gmail.com>
2026-09-06 12:34:28 -0600

    chore: remove outdated gig entries from September 4th and 5th

--- data.js
+++ data.js
@@ -41,24 +41,6 @@ const siteData = {
 
   gigs: [
     {
-      date: "Sept 4",
-      time: "9:30pm-1:30am",
-      venue: "Overtime Sports Bar",
-      location: "Win River Casino",
-      memo: "Opening weekend blowout in the casino lounge! Dynamic live sets.",
-      ticketLink: "",
-      ticketText: "See you there!"
-    },
-    {
-      date: "Sept 5",
-      time: "7pm-10pm",
-      venue: "Antlers Resort",
-      location: "20679 Antlers Rd, Lakehead, CA",
-      memo: "Labor Day weekend on the lake! 90s hits and originals!",
-      ticketLink: "",
-      ticketText: "See you there!"
-    },
-    {
       date: "Sept 11",
       time: "8pm-12am",
       venue: "Legion Hall",
